7b6b912ace8e0a4cf93b9db487f3ee6879de726a
tom <tomhaoye@163.com>
2017-08-02 00:47:52 +0800

    v3 done & fix bug

--- qrcode/qrcode_v3.php
+++ qrcode/qrcode_v3.php
@@ -11,10 +11,13 @@ function getStringInput()
         $s = fgets($input);
     }
     fclose($input);
-    return $s;
+    drawQRCode($s);
 }
 
-function drawQRCode($mask = 1)
+/**
+ * @param $data
+ */
+function drawQRCode($data)
 {
     $width = 21;
     $height = 21;
@@ -27,7 +30,7 @@ function drawQRCode($mask = 1)
 
     buildLocateSign($image, $width, $location_mod, $black, $white);
     buildTimeSign($image, $location_mod, $black);
-    encodeString($image, $white, $black, $mask);
+    $mask_id = encodeString($data, $image, $white, $black);
 
     //掩码ID
     $mask_code_arr = [
@@ -40,11 +43,11 @@ function drawQRCode($mask = 1)
         7 => '110',
         8 => '111',
     ];
-    $fmt_code = rsMask('01', intval($mask_code_arr[$mask], 2));
+    $fmt_code = rsMask('01', intval($mask_code_arr[$mask_id], 2));
     $fmt_code = array_reverse($fmt_code);
     fillFmt($image, $fmt_code, $white, $black);
 
-    exportPng($image, $mask);
+    exportPng($image, $mask_id);
 }
 
 /**
@@ -126,14 +129,14 @@ function buildTimeSign($image, $location_mod, $black)
  * 再把数据用 ISO/IEC 8859-1 标准编码，
  * 按八个二进制位分组，接上终止符和11101100和00010001交替的填充字节，按标准修剪到19字节，完成数据编码
  *
+ * @param $data
  * @param $image
  * @param $white
  * @param $black
- * @param int $mask
+ * @return int $mask_id
  */
-function encodeString($image, $white, $black, $mask = 1)
+function encodeString($data, $image, $white, $black)
 {
-    $data = 'hello mask!';
     $bit_string = '0100';
     $len = strlen($data);
     $bit_string .= sprintf("%08b", $len);
@@ -146,10 +149,13 @@ function encodeString($image, $white, $black, $mask = 1)
         $res[] = intval(substr($bit_string, $n, 8), 2);
     }
     while (count($res) < 19) {
-        $res[] = intval('11101100', 2);
-        $res[] = intval('00010001', 2);
+        if (count($res) == 18) {
+            $res[] = intval('11101100', 2);
+        } else {
+            $res[] = intval('11101100', 2);
+            $res[] = intval('00010001', 2);
+        }
     }
-    unset($res[19]);
     //加入容错编码 L 7
     $return = rsEncode($res, 7);
     foreach ($return as &$item) {
@@ -161,15 +167,29 @@ function encodeString($image, $white, $black, $mask = 1)
             $fill_arr[$key][$p] = $item[$p];
         }
     }
-
+    //數據部分編碼模板
     $template = template($fill_arr);
-    newFillData($image, $template, $mask, $white, $black);
+    $mask_code_arr = [1, 2, 3, 4, 5, 6, 7, 8];
 
+    //寻找最低惩罚依此mask生成二维码
+    $penalty = [];
+    $mask_template = [];
+    foreach ($mask_code_arr as $mask_id) {
+        $mask_template[$mask_id] = maskTemplate($template, $mask_id);
+        $penalty[$mask_id] = penalty($mask_template[$mask_id]);
+    }
+    $mask_id = array_search(min($penalty), $penalty);
+    $mask_template = $mask_template[$mask_id];
+
+    fillMaskData($image, $mask_template, $white, $black);
+
+    return $mask_id;
 }
 
 /**
  * todo v4 全模板
  *
+ * version1 qr code ad hoc
  * 构造模版(数据编码部分)
  * v1 qr-code 21*21 matrix
  * @param $fill_arr
@@ -187,7 +207,9 @@ function template($fill_arr)
     $y_operate = 1;
 
     for ($i = 0; $i < count($fill_arr); $i++) {
-        echo $x . ' ';
+
+        if ($x == $left_mid && $y_operate > 0) $y = 12;
+        elseif ($x == $left_mid && $y_operate < 0) $y = 9;
 
         for ($j = 0; $j < 8; $j++) {
             //y=6定时标示
@@ -204,17 +226,20 @@ function template($fill_arr)
         if ($x < $right_mid && $x > $left_mid) {
             $cell = 0;
         }
-        //y到顶或底 反响运算 x自减
-        if ($y <= $cell || $y >= $bottom) {
-            $y += $y_operate;
-            $y_operate = -$y_operate;
-            $x -= 2;
-        } elseif ($x <= $left_mid && $y_operate > 0) {
+
+        if ($x <= $left_mid && $y_operate > 0) {
             $y = 12;
             $x -= 2;
         } elseif ($x <= $left_mid && $y_operate < 0) {
             $y = 9;
             $x -= 2;
+        } else {
+            //y到顶或底 反响运算 x自减
+            if ($y <= $cell || $y >= $bottom) {
+                $y += $y_operate;
+                $y_operate = -$y_operate;
+                $x -= 2;
+            }
         }
         //x=6定时标示
         if ($x == 6) $x -= 1;
@@ -222,12 +247,38 @@ function template($fill_arr)
     return $template;
 }
 
-function newFillData($image, $template, $mask_id = 1, $white, $black)
+/**
+ * 计算掩码后的模板
+ * @param $template
+ * @param $mask_id
+ * @return array
+ */
+function maskTemplate($template, $mask_id)
 {
+    $mask_template = [];
     for ($i = 0; $i < 21; $i++) {
         for ($j = 0; $j < 21; $j++) {
             if (isset($template[$i][$j])) {
-                imagesetpixel($image, $i, $j, (!empty($template[$i][$j]) ^ mask($i, $j, $mask_id)) ? $black : $white);
+                $mask_template[$i][$j] = !empty($template[$i][$j]) ^ mask($i, $j, $mask_id);
+            }
+        }
+    }
+    return $mask_template;
+}
+
+/**
+ * 填寫編碼部分數據（使用被掩码编码的模板）
+ * @param $image
+ * @param $mask_template
+ * @param $white
+ * @param $black
+ */
+function fillMaskData($image, $mask_template, $white, $black)
+{
+    for ($i = 0; $i < 21; $i++) {
+        for ($j = 0; $j < 21; $j++) {
+            if (isset($mask_template[$i][$j])) {
+                imagesetpixel($image, $i, $j, (!empty($mask_template[$i][$j])) ? $black : $white);
             }
         }
     }
@@ -282,15 +333,125 @@ function mask($column, $row, $type)
     }
 }
 
-/*
- * todo 惩罚计算
+/**
+ * 懲罰計算
+ * Calculate penalty score for a masked matrix.
+ * N1: penalty for more than 5 consecutive pixels in row/column,
+ * 3 points for each occurrence of such pattern,
+ * and extra 1 point for each pixel exceeding 5
+ * consecutive pixels.
+ * N2: penalty for blocks of pixels larger than 2x2.
+ * 3*(m-1)*(n-1) points for each block of mxn
+ * (larger than 2x2).
+ * N3: penalty for patterns similar to the finder pattern.
+ * 40 points for each occurrence of 1:1:3:1:1 ratio
+ * (dark:light:dark:light:dark) pattern in row/column,
+ * preceded of followed by 4 consecutive light pixels.
+ * N4: penalty for unbalanced dark/light ratio.
+ * 10*k points where k is the rating of the deviation of
+ * the proportion of dark pixels from 50% in steps of 5%.
+ *
+ * @param $matrix
+ * @return int
  */
-function penalty()
+function penalty($matrix)
 {
+    $n1 = 0;
+    $n2 = 0;
+    $n3 = 0;
+    $n4 = 0;
+    //N1
+    for ($j = 0; $j < count($matrix); $j++) {
+        $count = 1;
+        $adj = false;
+        for ($i = 1; $i < count($matrix); $i++) {
+            if (isset($matrix[$j][$i - 1]) and isset($matrix[$j][$i]) and $matrix[$j][$i] == $matrix[$j][$i - 1]) {
+                $count += 1;
+            } else {
+                $count = 1;
+                $adj = false;
+            }
+            if ($count >= 5) {
+                if (!$adj) {
+                    $adj = true;
+                    $n1 += 3;
+                }
+            } else {
+                $n1 += 1;
+            }
+        }
+    }
+    for ($i = 0; $i < count($matrix); $i++) {
+        $count = 1;
+        $adj = false;
+        for ($j = 1; $j < count($matrix); $j++) {
+            if (isset($matrix[$j - 1][$i]) and isset($matrix[$j][$i]) and $matrix[$j][$i] == $matrix[$j - 1][$i]) {
+                $count += 1;
+            } else {
+                $count = 1;
+                $adj = false;
+            }
+            if ($count >= 5) {
+                if (!$adj) {
+                    $adj = true;
+                    $n1 += 3;
+                }
+            } else {
+                $n1 += 1;
+            }
+        }
+    }
 
+    //N2
+    $m = 1;
+    $n = 1;
+    for ($j = 1; $j < count($matrix); $j++) {
+        for ($i = 1; $i < count($matrix); $i++) {
+            if (isset($matrix[$j][$i]) and isset($matrix[$j - 1][$i]) and isset($matrix[$j][$i - 1]) and isset($matrix[$j - 1][$i - 1]) and $matrix[$j][$i] == $matrix[$j - 1][$i] and $matrix[$j][$i] == $matrix[$j][$i - 1] and $matrix[$j][$i] == $matrix[$j - 1][$i - 1]) {
+                if ($matrix[$j][$i] == $matrix[$j - 1][$i]) {
+                    $m += 1;
+                }
+                if ($matrix[$j][$i] == $matrix[$j][$i - 1]) {
+                    $n += 1;
+                }
+            } else {
+                $n2 += 3 * ($m - 1) * ($n - 1);
+                $m = 1;
+                $n = 1;
+            }
+        }
+    }
+
+    //todo N3
+
+
+    //N4
+    $dark = getSum($matrix);
+    $percent = intval((floatval($dark)) / floatval(count($matrix) ** 2) * 100);
+    $pre = $percent - $percent % 5;
+    $nex = $percent + 5 - $percent % 5;
+    $n4 = min(abs($pre - 50) / 5, abs($nex - 50) / 5) * 10;
+    return $n1 + $n2 + $n3 + $n4;
 }
 
-//格式标示
+function getSum($array)
+{
+    $num = 0;
+    foreach ($array as $k => $v) {
+        if (is_array($v)) {
+            $num += getSum($v);
+        }
+    }
+    return $num + array_sum($array);
+}
+
+/**
+ * 格式标示
+ * @param $image
+ * @param $arr
+ * @param $white
+ * @param $black
+ */
 function fillFmt($image, $arr, $white, $black)
 {
     for ($i = 0; $i <= 5; $i++)
@@ -315,16 +476,9 @@ function fillFmt($image, $arr, $white, $black)
 
 function exportPng($image, $mask_id)
 {
-    imagepng($image, $mask_id . 'v3_png.png');
+    imagepng($image, 'mask' . $mask_id . 'v3_png.png');
     imagedestroy($image);
 }
 
 
-drawQRCode(1);
-drawQRCode(2);
-drawQRCode(3);
-drawQRCode(4);
-drawQRCode(5);
-drawQRCode(6);
-drawQRCode(7);
-drawQRCode(8);
+getStringInput();
